d3b105affd8c843c401b08eaf7dde9f94155e847
mmoessler <markus.moessler@uni-hohenheim.de>
2023-05-30 11:13:32 +0200

    rearranged and updated files for ols case 2

---
+++ ols-case-02/myScript.js
@@ -0,0 +1,135 @@
+
+//..............................
+// handling of collapsibles
+// see: https://www.w3schools.com/howto/howto_js_collapsible.asp
+var coll = document.getElementsByClassName("collButtonCl");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
+}
+
+//..............................
+// handling of tabs      
+// see: https://www.w3schools.com/howto/howto_js_tabs.asp
+
+// open specific tab
+function openSpecificTab(tabContentId, tabLinkId, color) {
+
+// hide all elements with class="tabcontent_l1" by default */
+var i, tabcontent, tablinks;
+
+tabcontent = document.getElementsByClassName("tabContentL1Cl");
+for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+}
+
+// remove the background color of all tablinks/buttons
+tablinks = document.getElementsByClassName("tabLinkL1Cl");
+for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+}
+
+// show the specific tab content
+document.getElementById(tabContentId).style.display = "block";
+
+// add the specific color to the button used to open the tab content
+document.getElementById(tabLinkId).style.backgroundColor = color;
+
+}
+
+//..............................
+// handling of slider                
+// see: https://seiyria.com/bootstrap-slider/
+    
+var nVec = [5, 100]
+var xVec = [1, 10]
+var uVec = [1, 10]
+var gVec = [-0.2, 0.2]
+
+//var slider01 = new Slider("#ex601", { tooltip: 'always' });
+var slider01 = new Slider("#ex601", { 
+    tooltip: "always",
+    formatter: function(value) {
+        return "current value: " + nVec[value]
+    } 
+});
+//var slider02 = new Slider("#ex602", { tooltip: 'always' });
+var slider02 = new Slider("#ex602", { 
+    tooltip: "always",
+    formatter: function(value) {
+        return "current value: " + xVec[value]
+    } 
+});
+//var slider03 = new Slider("#ex603", { tooltip: 'always' });
+var slider03 = new Slider("#ex603", { 
+    tooltip: "always",
+    formatter: function(value) {
+        return "current value: " + uVec[value]
+    } 
+});
+//var slider04 = new Slider("#ex604", { tooltip: 'always' });
+var slider04 = new Slider("#ex604", { 
+    tooltip: "always",
+    formatter: function(value) {
+        return "current value: " + gVec[value]
+    } 
+});
+
+slider01.on("slide", function() {
+    var sliderValue01 = slider01.getValue() + 1;
+    var sliderValue02 = slider02.getValue() + 1;
+    var sliderValue03 = slider03.getValue() + 1;
+    var sliderValue04 = slider04.getValue() + 1;
+    document.getElementById("imageL1N1Id").setAttribute("src", "./figures/figure_01_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N2Id").setAttribute("src", "./figures/figure_02_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N3Id").setAttribute("src", "./figures/figure_03_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N4Id").setAttribute("src", "./figures/figure_04_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+
+    console.log("./figures/figure_01_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg")
+
+})
+
+slider02.on("slide", function() {
+    var sliderValue01 = slider01.getValue() + 1;
+    var sliderValue02 = slider02.getValue() + 1;
+    var sliderValue03 = slider03.getValue() + 1;
+    var sliderValue04 = slider04.getValue() + 1;
+    document.getElementById("imageL1N1Id").setAttribute("src", "./figures/figure_01_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N2Id").setAttribute("src", "./figures/figure_02_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N3Id").setAttribute("src", "./figures/figure_03_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N4Id").setAttribute("src", "./figures/figure_04_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+})
+
+slider03.on("slide", function() {
+    var sliderValue01 = slider01.getValue() + 1;
+    var sliderValue02 = slider02.getValue() + 1;
+    var sliderValue03 = slider03.getValue() + 1;
+    var sliderValue04 = slider04.getValue() + 1;
+    document.getElementById("imageL1N1Id").setAttribute("src", "./figures/figure_01_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N2Id").setAttribute("src", "./figures/figure_02_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N3Id").setAttribute("src", "./figures/figure_03_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N4Id").setAttribute("src", "./figures/figure_04_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+})
+
+slider04.on("slide", function() {
+    var sliderValue01 = slider01.getValue() + 1;
+    var sliderValue02 = slider02.getValue() + 1;
+    var sliderValue03 = slider03.getValue() + 1;
+    var sliderValue04 = slider04.getValue() + 1;
+    document.getElementById("imageL1N1Id").setAttribute("src", "./figures/figure_01_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N2Id").setAttribute("src", "./figures/figure_02_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N3Id").setAttribute("src", "./figures/figure_03_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+    document.getElementById("imageL1N4Id").setAttribute("src", "./figures/figure_04_" + sliderValue01 + "_" + sliderValue02 + "_" + sliderValue03 + "_" + sliderValue04 + ".svg");
+})
+
+// set start page
+document.getElementById("tabLinkL1N1Id").click();
