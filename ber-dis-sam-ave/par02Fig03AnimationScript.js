6a4819ada8961b6c67ddac0f2e0db0d75a78f2b9
mmoessler <markus.moessler@uni-hohenheim.de>
2023-07-19 15:10:54 +0200

    excluded general animation js for 2 parameters and 3 figures

---
+++ ber-dis-sam-ave/par02Fig03AnimationScript.js
@@ -0,0 +1,78 @@
+
+// loop over 2 slider and 2 figures
+function sliderLoop(loopSliderValue01, loopSliderValue02) {
+
+    var sliderValue01 = loopSliderValue01 + 1;
+    var sliderValue02 = loopSliderValue02 + 1;
+
+    document.getElementById("imageL1N1Id").setAttribute("src", "./figures/figure_01_" + sliderValue01 + "_" + sliderValue02 + ".svg");
+    document.getElementById("imageL1N2Id").setAttribute("src", "./figures/figure_02_" + sliderValue01 + "_" + sliderValue02 + ".svg");
+    document.getElementById("imageL1N3Id").setAttribute("src", "./figures/figure_03_" + sliderValue01 + "_" + sliderValue02 + ".svg");
+
+    document.getElementById("sliderValue01Id").innerHTML = par01Str01 + par01Vec[slider01.getValue()] + "\\)";
+    var element = document.getElementById("sliderValue01Id");
+    MathJax.typeset([element]);
+
+    document.getElementById("sliderValue02Id").innerHTML = par02Str01 + par02Vec[slider02.getValue()] + "\\)";
+    var element = document.getElementById("sliderValue02Id");
+    MathJax.typeset([element]);
+
+    var slider = document.getElementById("slider01Id");
+    slider.value = sliderValue01;
+
+    var slider = document.getElementById("slider02Id");
+    slider.value = sliderValue02;
+
+};
+
+// explanation button
+explainButto01Click = function(audioId) {
+  var audio = document.getElementById(audioId);
+  audio.play();
+}
+
+// animate silder 1 out of 2
+animateButto01Click = function(org, start, stop, audioId) {
+  var audio = document.getElementById(audioId);
+  audio.play();
+  var ind = start;
+  var loopSliderValue02 = slider02.getValue();
+  var outerInterval = setInterval(function() {
+    var loopSliderValue01 = ind;
+    slider01.setValue(ind);
+    sliderLoop(loopSliderValue01, loopSliderValue02);
+    ind++;
+    if (ind > stop) {
+      var innerInterval = setInterval(function() {
+        slider01.setValue(ind);
+        slider01.setValue(org);
+        sliderLoop(org, loopSliderValue02);
+        clearInterval(innerInterval);
+      }, 1000);
+      clearInterval(outerInterval);
+    }
+  }, 1000);
+}
+
+// animate silder 2 out of 2
+animateButto02Click = function(org, start, stop, audioId) {    
+  var audio = document.getElementById(audioId);
+  audio.play();
+  var ind = start;
+  var loopSliderValue01 = slider01.getValue();
+  var outerInterval = setInterval(function() {
+    var loopSliderValue02 = ind;
+    slider02.setValue(ind);
+    sliderLoop(loopSliderValue01, loopSliderValue02);
+    ind++;
+    if (ind > stop) {
+      var innerInterval = setInterval(function() {
+        slider02.setValue(ind);
+        slider02.setValue(org);            
+        sliderLoop(loopSliderValue01, org);
+        clearInterval(innerInterval);
+      }, 1000);        
+      clearInterval(outerInterval);
+    }
+  }, 1000);
+}
