3ef5444f303d39a3836d0e7a83f7e8aa32a5bbf0
mmoessler <markus.moessler@gmail.com>
2023-07-20 13:25:08 +0200

    some minor changes

--- ber-dis-sam-ave/myScript.js
+++ ber-dis-sam-ave/myScript.js
@@ -6,15 +6,15 @@ var coll = document.getElementsByClassName("collButtonCl");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-        content.style.display = "none";
-        } else {
-        content.style.display = "block";
-        }
-    });
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+      }
+  });
 }
 
 //..............................
