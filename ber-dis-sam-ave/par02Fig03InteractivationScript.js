1aa716464cada6ee41bed9c166c28165bc6335d4
mmoessler <markus.moessler@uni-hohenheim.de>
2023-07-21 13:15:39 +0200

    added code to display initial figures

--- ber-dis-sam-ave/par02Fig03InteractivationScript.js
+++ ber-dis-sam-ave/par02Fig03InteractivationScript.js
@@ -1,4 +1,9 @@
 
+// display initial figures
+document.getElementById("imageL1N1Id").setAttribute("src", "./figures/figure_01_" + par01Ini + "_" + par02Ini + ".svg");
+document.getElementById("imageL1N2Id").setAttribute("src", "./figures/figure_02_" + par01Ini + "_" + par02Ini + ".svg");
+document.getElementById("imageL1N3Id").setAttribute("src", "./figures/figure_03_" + par01Ini + "_" + par02Ini + ".svg");
+
 // set initial value of slider 1
 document.getElementById("sliderValue01Id").innerHTML = par01Str01 + par01Vec[par01Ini] + "\\)";
 MathJax.typeset([document.getElementById("sliderValue01Id")]);
