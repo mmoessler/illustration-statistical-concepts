2d63f28329d9971c1e17a18aaf123facbf95bf3e
mmoessler <markus.moessler@uni-hohenheim.de>
2023-07-26 14:10:22 +0200

    added js for read out api

--- ber-dis-sam-ave/par02Fig03AnimationScript.js
+++ ber-dis-sam-ave/par02Fig03AnimationScript.js
@@ -1,5 +1,5 @@
 
-// loop over 2 slider and 2 figures
+// loop over 2 slider and 4 figures
 function sliderLoop(loopSliderValue01, loopSliderValue02) {
 
   var sliderValue01 = loopSliderValue01 + 1;
@@ -10,10 +10,10 @@ function sliderLoop(loopSliderValue01, loopSliderValue02) {
   document.getElementById("image3Id").setAttribute("src", "./figures/figure_03_" + sliderValue01 + "_" + sliderValue02 + ".svg");
 
   document.getElementById("sliderValue1Id").innerHTML = par01Str01 + par01Vec[slider01.getValue()] + "\\)";
-  MathJax.typeset([element = document.getElementById("sliderValue1Id")]);
+  MathJax.typeset([document.getElementById("sliderValue1Id")]);
 
   document.getElementById("sliderValue2Id").innerHTML = par02Str01 + par02Vec[slider02.getValue()] + "\\)";
-  MathJax.typeset([element = document.getElementById("sliderValue2Id")]);
+  MathJax.typeset([document.getElementById("sliderValue2Id")]);
 
   document.getElementById("slider1Id").value = sliderValue01;
   document.getElementById("slider2Id").value = sliderValue02;
@@ -24,15 +24,58 @@ function sliderLoop(loopSliderValue01, loopSliderValue02) {
 explainButton01Click = function() {
 
   if (activeTabId == "tabContentL1N1Id") {
-    var audio = document.getElementById("audioFigure01OverallId");
+    var audioTextDiv = document.getElementById("audioTextFigure01OverallId");
+    var audioShowPar = document.getElementById("audioShowTextFigure01Id");
   } else if (activeTabId == "tabContentL1N2Id") {
-    var audio = document.getElementById("audioFigure02OverallId");
+    var audioTextDiv = document.getElementById("audioTextFigure02OverallId");
+    var audioShowPar = document.getElementById("audioShowTextFigure02Id");
   } else if (activeTabId == "tabContentL1N3Id") {
-    var audio = document.getElementById("audioFigure03OverallId");
+    var audioTextDiv = document.getElementById("audioTextFigure03OverallId");
+    var audioShowPar = document.getElementById("audioShowTextFigure03Id");
   } else {
-    var audio = document.getElementById("audioErrorId");
+    var audioTextDiv = document.getElementById("audioTextNoText");
   }
-  audio.play();
+  
+  audioShowPar.innerHTML = audioTextDiv.innerHTML;
+  audioShowPar.style.display = "block";
+
+  var silentSpan = "audioTextSilentCl"; 
+  var audioTextSpa = audioTextDiv.getElementsByTagName("span");
+
+  var index = 0;
+
+  // function to read out each <span> with a delay in between
+  function readSpans() {
+
+    if (index < audioTextSpa.length) {
+
+      var span = audioTextSpa[index];
+      var speech = new SpeechSynthesisUtterance("whatever");
+
+      if (!span.classList.contains(silentSpan)) {
+
+        var spanText = span.textContent;
+        var speech = new SpeechSynthesisUtterance(spanText);
+        speechSynthesis.speak(speech);
+
+        speech.onend = function() {
+          setTimeout(function() {
+            index++;
+            readSpans();
+          }, 1000);
+        };
+        
+      } else {
+        index++;
+        readSpans();
+      }
+    } else {      
+      audioShowPar.style.display = "none";
+    }
+  }
+
+  // start reading out the spans
+  readSpans();
   
 }
 
@@ -40,15 +83,60 @@ explainButton01Click = function() {
 animateButton01Click = function(org, start, stop) {
 
   if (activeTabId == "tabContentL1N1Id") {
-    var audio = document.getElementById("audioFigure01Slider1Id");
+    var audioTextDiv = document.getElementById("audioTextFigure01Slider1Id");
+    var audioShowPar = document.getElementById("audioShowTextFigure01Id");
   } else if (activeTabId == "tabContentL1N2Id") {
-    var audio = document.getElementById("audioFigure02Slider1Id");
+    var audioTextDiv = document.getElementById("audioTextFigure02Slider1Id");
+    var audioShowPar = document.getElementById("audioShowTextFigure02Id");
   } else if (activeTabId == "tabContentL1N3Id") {
-    var audio = document.getElementById("audioFigure03Slider1Id");
+    var audioTextDiv = document.getElementById("audioTextFigure03Slider1Id");
+    var audioShowPar = document.getElementById("audioShowTextFigure03Id");
   } else {
-    var audio = document.getElementById("audioErrorId");
+    var audioTextDiv = document.getElementById("audioTextNoText");
   }
-  audio.play();
+
+  audioShowPar.innerHTML = audioTextDiv.innerHTML;
+  audioShowPar.style.display = "block";
+
+  var silentSpan = "audioTextSilentCl"; 
+  var audioTextSpa = audioTextDiv.getElementsByTagName("span");
+
+  var index = 0;
+
+  // function to read out each <span> with a delay in between
+  function readSpans() {
+
+    if (index < audioTextSpa.length) {
+
+      var span = audioTextSpa[index];
+      var speech = new SpeechSynthesisUtterance("whatever");
+
+      if (!span.classList.contains(silentSpan)) {
+
+        var spanText = span.textContent;
+        var speech = new SpeechSynthesisUtterance(spanText);
+        speechSynthesis.speak(speech);
+
+        speech.onend = function() {
+          setTimeout(function() {
+            index++;
+            readSpans();
+          }, 1000);
+        };
+        
+      } else {
+        index++;
+        readSpans();
+      }
+    } else {      
+      audioShowPar.style.display = "none";
+    }
+  }
+
+  // start reading out the spans
+  readSpans();
+  
+  // iteration over sliders
   var ind = start;
   var loopSliderValue02 = slider02.getValue();
   var outerInterval = setInterval(function() {
@@ -69,18 +157,63 @@ animateButton01Click = function(org, start, stop) {
 }
 
 // animate silder 2 out of 2
-animateButton02Click = function(org, start, stop) {
+animateButton02Click = function(org, start, stop) {   
 
   if (activeTabId == "tabContentL1N1Id") {
-    var audio = document.getElementById("audioFigure01Slider2Id");
+    var audioTextDiv = document.getElementById("audioTextFigure01Slider2Id");
+    var audioShowPar = document.getElementById("audioShowTextFigure01Id");
   } else if (activeTabId == "tabContentL1N2Id") {
-    var audio = document.getElementById("audioFigure02Slider2Id");
+    var audioTextDiv = document.getElementById("audioTextFigure02Slider2Id");
+    var audioShowPar = document.getElementById("audioShowTextFigure02Id");
   } else if (activeTabId == "tabContentL1N3Id") {
-    var audio = document.getElementById("audioFigure03Slider2Id");
+    var audioTextDiv = document.getElementById("audioTextFigure03Slider2Id");
+    var audioShowPar = document.getElementById("audioShowTextFigure03Id");
   } else {
-    var audio = document.getElementById("audioErrorId");
+    var audioTextDiv = document.getElementById("audioTextNoText");
   }
-  audio.play();
+
+  audioShowPar.innerHTML = audioTextDiv.innerHTML;
+  audioShowPar.style.display = "block";
+
+  var silentSpan = "audioTextSilentCl"; 
+  var audioTextSpa = audioTextDiv.getElementsByTagName("span");
+
+  var index = 0;
+
+  // function to read out each <span> with a delay in between
+  function readSpans() {
+
+    if (index < audioTextSpa.length) {
+
+      var span = audioTextSpa[index];
+      var speech = new SpeechSynthesisUtterance("whatever");
+
+      if (!span.classList.contains(silentSpan)) {
+
+        var spanText = span.textContent;
+        var speech = new SpeechSynthesisUtterance(spanText);
+        speechSynthesis.speak(speech);
+
+        speech.onend = function() {
+          setTimeout(function() {
+            index++;
+            readSpans();
+          }, 1000);
+        };
+        
+      } else {
+        index++;
+        readSpans();
+      }
+    } else {      
+      audioShowPar.style.display = "none";
+    }
+  }
+
+  // start reading out the spans
+  readSpans();
+  
+  // iteration over sliders
   var ind = start;
   var loopSliderValue01 = slider01.getValue();
   var outerInterval = setInterval(function() {
