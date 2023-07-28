7ac2c5d4dd3cc03266710df2621fcec670d63b96
mmoessler <markus.moessler@uni-hohenheim.de>
2023-07-28 08:29:14 +0200

    upgraded animation function to slide until speech ends

--- con-uni-dis-sam-ave/par03Fig03AnimationScript.js
+++ con-uni-dis-sam-ave/par03Fig03AnimationScript.js
@@ -84,8 +84,244 @@ explainButton01Click = function() {
   
 }
 
+// // animate slider 1 out of 3
+// animateButton01Click = function(org, start, stop) {
+
+//   if (activeTabId == "tabContentL1N1Id") {
+//     var audioTextDiv = document.getElementById("audioTextFigure01Slider1Id");
+//     var audioShowPar = document.getElementById("audioShowTextFigure01Id");
+//   } else if (activeTabId == "tabContentL1N2Id") {
+//     var audioTextDiv = document.getElementById("audioTextFigure02Slider1Id");
+//     var audioShowPar = document.getElementById("audioShowTextFigure02Id");
+//   } else if (activeTabId == "tabContentL1N3Id") {
+//     var audioTextDiv = document.getElementById("audioTextFigure03Slider1Id");
+//     var audioShowPar = document.getElementById("audioShowTextFigure03Id");
+//   } else {
+//     var audioTextDiv = document.getElementById("audioTextNoText");
+//   }
+
+//   audioShowPar.innerHTML = audioTextDiv.innerHTML;
+//   audioShowPar.style.display = "block";
+
+//   var silentSpan = "audioTextSilentCl"; 
+//   var audioTextSpa = audioTextDiv.getElementsByTagName("span");
+
+//   var index = 0;
+
+//   // function to read out each <span> with a delay in between
+//   function readSpans() {
+
+//     if (index < audioTextSpa.length) {
+
+//       var span = audioTextSpa[index];
+//       var speech = new SpeechSynthesisUtterance("whatever");
+
+//       if (!span.classList.contains(silentSpan)) {
+
+//         var spanText = span.textContent;
+//         var speech = new SpeechSynthesisUtterance(spanText);
+//         speechSynthesis.speak(speech);
+
+//         speech.onend = function() {
+//           setTimeout(function() {
+//             index++;
+//             readSpans();
+//           }, 1000);
+//         };
+        
+//       } else {
+//         index++;
+//         readSpans();
+//       }
+//     } else {      
+//       audioShowPar.style.display = "none";
+//     }
+//   }
+
+//   // start reading out the spans
+//   readSpans();
+  
+//   // iteration over sliders
+//   var ind = start;
+//   var loopSliderValue02 = slider02.getValue();
+//   var loopSliderValue03 = slider03.getValue();
+//   var outerInterval = setInterval(function() {
+//     var loopSliderValue01 = ind;
+//     slider01.setValue(ind);
+//     sliderLoop(loopSliderValue01, loopSliderValue02, loopSliderValue03);
+//     ind++;    
+//     if (ind > stop) {
+//       var innerInterval = setInterval(function() {
+//         slider01.setValue(ind);
+//         slider01.setValue(org);
+//         sliderLoop(org, loopSliderValue02, loopSliderValue03);
+//         clearInterval(innerInterval);
+//       }, 1000);
+//       clearInterval(outerInterval);      
+//     }    
+//   }, 1000);
+// }
+
+// // animate slider 2 out of 3
+// animateButton02Click = function(org, start, stop) {   
+
+//   if (activeTabId == "tabContentL1N1Id") {
+//     var audioTextDiv = document.getElementById("audioTextFigure01Slider2Id");
+//     var audioShowPar = document.getElementById("audioShowTextFigure01Id");
+//   } else if (activeTabId == "tabContentL1N2Id") {
+//     var audioTextDiv = document.getElementById("audioTextFigure02Slider2Id");
+//     var audioShowPar = document.getElementById("audioShowTextFigure02Id");
+//   } else if (activeTabId == "tabContentL1N3Id") {
+//     var audioTextDiv = document.getElementById("audioTextFigure03Slider2Id");
+//     var audioShowPar = document.getElementById("audioShowTextFigure03Id");
+//   } else {
+//     var audioTextDiv = document.getElementById("audioTextNoText");
+//   }
+
+//   audioShowPar.innerHTML = audioTextDiv.innerHTML;
+//   audioShowPar.style.display = "block";
+
+//   var silentSpan = "audioTextSilentCl"; 
+//   var audioTextSpa = audioTextDiv.getElementsByTagName("span");
+
+//   var index = 0;
+
+//   // function to read out each <span> with a delay in between
+//   function readSpans() {
+
+//     if (index < audioTextSpa.length) {
+
+//       var span = audioTextSpa[index];
+//       var speech = new SpeechSynthesisUtterance("whatever");
+
+//       if (!span.classList.contains(silentSpan)) {
+
+//         var spanText = span.textContent;
+//         var speech = new SpeechSynthesisUtterance(spanText);
+//         speechSynthesis.speak(speech);
+
+//         speech.onend = function() {
+//           setTimeout(function() {
+//             index++;
+//             readSpans();
+//           }, 1000);
+//         };
+        
+//       } else {
+//         index++;
+//         readSpans();
+//       }
+//     } else {      
+//       audioShowPar.style.display = "none";
+//     }
+//   }
+
+//   // start reading out the spans
+//   readSpans();
+  
+//   // iteration over sliders
+//   var ind = start;
+//   var loopSliderValue01 = slider01.getValue();
+//   var loopSliderValue03 = slider03.getValue();
+//   var outerInterval = setInterval(function() {
+//     var loopSliderValue02 = ind;
+//     slider02.setValue(ind);
+//     sliderLoop(loopSliderValue01, loopSliderValue02, loopSliderValue03);
+//     ind++;
+//     if (ind > stop) {
+//       var innerInterval = setInterval(function() {
+//         slider02.setValue(ind);
+//         slider02.setValue(org);            
+//         sliderLoop(loopSliderValue01, org, loopSliderValue03);
+//         clearInterval(innerInterval);
+//       }, 1000);        
+//       clearInterval(outerInterval);
+//     }
+//   }, 1000);
+// }
+
+// // animate slider 3 out of 3
+// animateButton03Click = function(org, start, stop) {   
+
+//   if (activeTabId == "tabContentL1N1Id") {
+//     var audioTextDiv = document.getElementById("audioTextFigure01Slider3Id");
+//     var audioShowPar = document.getElementById("audioShowTextFigure01Id");
+//   } else if (activeTabId == "tabContentL1N2Id") {
+//     var audioTextDiv = document.getElementById("audioTextFigure02Slider3Id");
+//     var audioShowPar = document.getElementById("audioShowTextFigure02Id");
+//   } else if (activeTabId == "tabContentL1N3Id") {
+//     var audioTextDiv = document.getElementById("audioTextFigure03Slider3Id");
+//     var audioShowPar = document.getElementById("audioShowTextFigure03Id");
+//   } else {
+//     var audioTextDiv = document.getElementById("audioTextNoText");
+//   }
+
+//   audioShowPar.innerHTML = audioTextDiv.innerHTML;
+//   audioShowPar.style.display = "block";
+
+//   var silentSpan = "audioTextSilentCl"; 
+//   var audioTextSpa = audioTextDiv.getElementsByTagName("span");
+
+//   var index = 0;
+
+//   // function to read out each <span> with a delay in between
+//   function readSpans() {
+
+//     if (index < audioTextSpa.length) {
+
+//       var span = audioTextSpa[index];
+//       var speech = new SpeechSynthesisUtterance("whatever");
+
+//       if (!span.classList.contains(silentSpan)) {
+
+//         var spanText = span.textContent;
+//         var speech = new SpeechSynthesisUtterance(spanText);
+//         speechSynthesis.speak(speech);
+
+//         speech.onend = function() {
+//           setTimeout(function() {
+//             index++;
+//             readSpans();
+//           }, 1000);
+//         };
+        
+//       } else {
+//         index++;
+//         readSpans();
+//       }
+//     } else {      
+//       audioShowPar.style.display = "none";
+//     }
+//   }
+
+//   // start reading out the spans
+//   readSpans();
+  
+//   // iteration over sliders
+//   var ind = start;
+//   var loopSliderValue01 = slider01.getValue();
+//   var loopSliderValue02 = slider02.getValue();
+//   var outerInterval = setInterval(function() {
+//     var loopSliderValue03 = ind;
+//     slider03.setValue(ind);
+//     sliderLoop(loopSliderValue01, loopSliderValue02, loopSliderValue03);
+//     ind++;
+//     if (ind > stop) {
+//       var innerInterval = setInterval(function() {
+//         slider03.setValue(ind);
+//         slider03.setValue(org);            
+//         sliderLoop(loopSliderValue01, loopSliderValue02, org);
+//         clearInterval(innerInterval);
+//       }, 1000);        
+//       clearInterval(outerInterval);
+//     }
+//   }, 1000);
+// }
+
+
+
 // animate slider 1 out of 3
-animateButton01Click = function(org, start, stop) {
+animateButton01Click = function() {   
 
   if (activeTabId == "tabContentL1N1Id") {
     var audioTextDiv = document.getElementById("audioTextFigure01Slider1Id");
@@ -106,64 +342,78 @@ animateButton01Click = function(org, start, stop) {
   var silentSpan = "audioTextSilentCl"; 
   var audioTextSpa = audioTextDiv.getElementsByTagName("span");
 
+  var stopLoop = false;
   var index = 0;
 
   // function to read out each <span> with a delay in between
-  function readSpans() {
-
+  function readSpans() {      
     if (index < audioTextSpa.length) {
-
       var span = audioTextSpa[index];
       var speech = new SpeechSynthesisUtterance("whatever");
-
       if (!span.classList.contains(silentSpan)) {
-
         var spanText = span.textContent;
         var speech = new SpeechSynthesisUtterance(spanText);
         speechSynthesis.speak(speech);
-
         speech.onend = function() {
           setTimeout(function() {
             index++;
             readSpans();
           }, 1000);
-        };
-        
+        };          
       } else {
         index++;
         readSpans();
       }
-    } else {      
-      audioShowPar.style.display = "none";
+    // finished with reading out spans
+    } else {
+      audioShowPar.style.display = "none";  
+      var intervalLoop = setInterval(function() {
+        stopLoop = true;      
+        slider01.setValue(slider01.options.value);
+        sliderLoop(slider01.getValue(), slider02.getValue(), slider03.getValue());
+        clearInterval(intervalLoop);      
+      }, 1000);
     }
   }
-
   // start reading out the spans
   readSpans();
   
-  // iteration over sliders
-  var ind = start;
-  var loopSliderValue02 = slider02.getValue();
-  var loopSliderValue03 = slider03.getValue();
-  var outerInterval = setInterval(function() {
-    var loopSliderValue01 = ind;
-    slider01.setValue(ind);
-    sliderLoop(loopSliderValue01, loopSliderValue02, loopSliderValue03);
-    ind++;    
-    if (ind > stop) {
-      var innerInterval = setInterval(function() {
-        slider01.setValue(ind);
-        slider01.setValue(org);
-        sliderLoop(org, loopSliderValue02, loopSliderValue03);
-        clearInterval(innerInterval);
-      }, 1000);
-      clearInterval(outerInterval);      
-    }    
-  }, 1000);
+  // iteration over sliders (sider specific)
+  var direction = 1; // 1 for increment, -1 for decrement
+  var minValue = slider01.options.min;
+  var maxValue = slider01.options.max;
+
+  var loopInterval = setInterval(function() {
+    // Get the current value of the slider
+    var value = slider01.getValue();  
+    // Do something with the current value (you can replace this with your custom logic)
+
+    //sliderLoop(slider01.getValue(), value);
+    
+    // Check if the event to stop the loop has been triggered
+    if (stopLoop) {
+      slider01.setValue(slider01.options.value);
+      sliderLoop(slider01.getValue(), slider02.getValue(), slider03.getValue());
+      clearInterval(loopInterval);      
+    } else {
+      // Check if the slider value reaches the minimum or maximum
+      if (value === maxValue || value === minValue) {
+          // Change the direction to move the slider back and forth
+          direction *= -1;
+      }
+      // Increment or decrement the slider value based on the direction
+      slider01.setValue(value + direction);
+
+      sliderLoop(slider01.getValue(), slider02.getValue(), slider02.getValue());
+
+    }
+
+  }, 1000); // Adjust the interval as needed (e.g., 100ms for a fast loop)
+
 }
 
 // animate slider 2 out of 3
-animateButton02Click = function(org, start, stop) {   
+animateButton02Click = function() {   
 
   if (activeTabId == "tabContentL1N1Id") {
     var audioTextDiv = document.getElementById("audioTextFigure01Slider2Id");
@@ -184,64 +434,78 @@ animateButton02Click = function(org, start, stop) {
   var silentSpan = "audioTextSilentCl"; 
   var audioTextSpa = audioTextDiv.getElementsByTagName("span");
 
+  var stopLoop = false;
   var index = 0;
 
   // function to read out each <span> with a delay in between
-  function readSpans() {
-
+  function readSpans() {      
     if (index < audioTextSpa.length) {
-
       var span = audioTextSpa[index];
       var speech = new SpeechSynthesisUtterance("whatever");
-
       if (!span.classList.contains(silentSpan)) {
-
         var spanText = span.textContent;
         var speech = new SpeechSynthesisUtterance(spanText);
         speechSynthesis.speak(speech);
-
         speech.onend = function() {
           setTimeout(function() {
             index++;
             readSpans();
           }, 1000);
-        };
-        
+        };          
       } else {
         index++;
         readSpans();
       }
-    } else {      
-      audioShowPar.style.display = "none";
+    // finished with reading out spans
+    } else {
+      audioShowPar.style.display = "none";  
+      var intervalLoop = setInterval(function() {
+        stopLoop = true;      
+        slider02.setValue(slider02.options.value);
+        sliderLoop(slider01.getValue(), slider02.getValue(), slider03.getValue());
+        clearInterval(intervalLoop);      
+      }, 1000);
     }
   }
-
   // start reading out the spans
   readSpans();
   
-  // iteration over sliders
-  var ind = start;
-  var loopSliderValue01 = slider01.getValue();
-  var loopSliderValue03 = slider03.getValue();
-  var outerInterval = setInterval(function() {
-    var loopSliderValue02 = ind;
-    slider02.setValue(ind);
-    sliderLoop(loopSliderValue01, loopSliderValue02, loopSliderValue03);
-    ind++;
-    if (ind > stop) {
-      var innerInterval = setInterval(function() {
-        slider02.setValue(ind);
-        slider02.setValue(org);            
-        sliderLoop(loopSliderValue01, org, loopSliderValue03);
-        clearInterval(innerInterval);
-      }, 1000);        
-      clearInterval(outerInterval);
+  // iteration over sliders (sider specific)
+  var direction = 1; // 1 for increment, -1 for decrement
+  var minValue = slider02.options.min;
+  var maxValue = slider02.options.max;
+
+  var loopInterval = setInterval(function() {
+    // Get the current value of the slider
+    var value = slider02.getValue();  
+    // Do something with the current value (you can replace this with your custom logic)
+
+    //sliderLoop(slider01.getValue(), value);
+    
+    // Check if the event to stop the loop has been triggered
+    if (stopLoop) {
+      slider02.setValue(slider02.options.value);
+      sliderLoop(slider01.getValue(), slider02.getValue(), slider03.getValue());
+      clearInterval(loopInterval);      
+    } else {
+      // Check if the slider value reaches the minimum or maximum
+      if (value === maxValue || value === minValue) {
+          // Change the direction to move the slider back and forth
+          direction *= -1;
+      }
+      // Increment or decrement the slider value based on the direction
+      slider02.setValue(value + direction);
+
+      sliderLoop(slider01.getValue(), slider02.getValue(), slider03.getValue());
+
     }
-  }, 1000);
+
+  }, 1000); // Adjust the interval as needed (e.g., 100ms for a fast loop)
+
 }
 
 // animate slider 3 out of 3
-animateButton03Click = function(org, start, stop) {   
+animateButton03Click = function() {   
 
   if (activeTabId == "tabContentL1N1Id") {
     var audioTextDiv = document.getElementById("audioTextFigure01Slider3Id");
@@ -262,58 +526,72 @@ animateButton03Click = function(org, start, stop) {
   var silentSpan = "audioTextSilentCl"; 
   var audioTextSpa = audioTextDiv.getElementsByTagName("span");
 
+  var stopLoop = false;
   var index = 0;
 
   // function to read out each <span> with a delay in between
-  function readSpans() {
-
+  function readSpans() {      
     if (index < audioTextSpa.length) {
-
       var span = audioTextSpa[index];
       var speech = new SpeechSynthesisUtterance("whatever");
-
       if (!span.classList.contains(silentSpan)) {
-
         var spanText = span.textContent;
         var speech = new SpeechSynthesisUtterance(spanText);
         speechSynthesis.speak(speech);
-
         speech.onend = function() {
           setTimeout(function() {
             index++;
             readSpans();
           }, 1000);
-        };
-        
+        };          
       } else {
         index++;
         readSpans();
       }
-    } else {      
-      audioShowPar.style.display = "none";
+    // finished with reading out spans
+    } else {
+      audioShowPar.style.display = "none";  
+      var intervalLoop = setInterval(function() {
+        stopLoop = true;      
+        slider03.setValue(slider03.options.value);
+        sliderLoop(slider01.getValue(), slider02.getValue(), slider03.getValue());
+        clearInterval(intervalLoop);      
+      }, 1000);
     }
   }
-
   // start reading out the spans
   readSpans();
   
-  // iteration over sliders
-  var ind = start;
-  var loopSliderValue01 = slider01.getValue();
-  var loopSliderValue02 = slider02.getValue();
-  var outerInterval = setInterval(function() {
-    var loopSliderValue03 = ind;
-    slider03.setValue(ind);
-    sliderLoop(loopSliderValue01, loopSliderValue02, loopSliderValue03);
-    ind++;
-    if (ind > stop) {
-      var innerInterval = setInterval(function() {
-        slider03.setValue(ind);
-        slider03.setValue(org);            
-        sliderLoop(loopSliderValue01, loopSliderValue02, org);
-        clearInterval(innerInterval);
-      }, 1000);        
-      clearInterval(outerInterval);
+  // iteration over sliders (sider specific)
+  var direction = 1; // 1 for increment, -1 for decrement
+  var minValue = slider03.options.min;
+  var maxValue = slider03.options.max;
+
+  var loopInterval = setInterval(function() {
+    // Get the current value of the slider
+    var value = slider03.getValue();  
+    // Do something with the current value (you can replace this with your custom logic)
+
+    //sliderLoop(slider01.getValue(), value);
+    
+    // Check if the event to stop the loop has been triggered
+    if (stopLoop) {
+      slider03.setValue(slider03.options.value);
+      sliderLoop(slider01.getValue(), slider02.getValue(), slider03.getValue());
+      clearInterval(loopInterval);      
+    } else {
+      // Check if the slider value reaches the minimum or maximum
+      if (value === maxValue || value === minValue) {
+          // Change the direction to move the slider back and forth
+          direction *= -1;
+      }
+      // Increment or decrement the slider value based on the direction
+      slider03.setValue(value + direction);
+
+      sliderLoop(slider01.getValue(), slider02.getValue(), slider03.getValue());
+
     }
-  }, 1000);
+
+  }, 1000); // Adjust the interval as needed (e.g., 100ms for a fast loop)
+
 }
