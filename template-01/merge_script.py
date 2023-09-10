0b4fa2da5aca5e17b83d3276be23733a0dc795b1
mmoessler <markus.moessler@gmail.com>
2023-09-10 21:32:11 +0200

    added script to add audiotext depending on numer of figures and sliders

--- template-01/merge_script.py
+++ template-01/merge_script.py
@@ -2,18 +2,37 @@
 # import modules
 import re
 
-# define functions
+# funtion to generate slider
 def generate_slider(slider_header, button_header, slider_id, slider_value_id, slider_value_max, slider_value, button_id):
-    tag = \
-        " \
-        <p style='font-size: 12pt; text-align: center;'>" + slider_header + "</p><p style='font-size: 12pt; text-align: center' class='sliderValueCl' id='sliderValue" + slider_value_id + "Id'></p> \
-        <input class='sliderCl' id='slider" + slider_id + "Id' type='text' data-slider-min='0' data-slider-max='" + slider_value_max + "' data-slider-step='1' data-slider-value='" + slider_value + "'/> \
-        <div style='font-size: 12pt; text-align: center;'> \
-        <button class='animateButtonCl' id='" + button_id + "' onclick='animateButtonClick(slider = " + slider_id + ")'>" + button_header + "</button> \
-        </div>"
-    return(tag)
+    tag = f"""
+        <p style='font-size: 12pt; text-align: center;'>{slider_header}</p>
+        <p style='font-size: 12pt; text-align: center' class='sliderValueCl' id='sliderValue{slider_value_id}Id'></p>
+        <input class='sliderCl' id='slider{slider_id}Id' type='text' data-slider-min='0' data-slider-max='{slider_value_max}' data-slider-step='1' data-slider-value='{slider_value}'/>
+        <div style='font-size: 12pt; text-align: center;'>
+            <button class='animateButtonCl' id='{button_id}' onclick='animateButtonClick(slider = {slider_id})'>{button_header}</button>
+        </div>
+    """
+    return tag
 
-# print(generate_slider(slider_header = "Sample size \(n\)", button_header = "Animate \(n\)", slider_id = "1", slider_value_id = "1", slider_value_max = "4", slider_value = "2", button_id = "1"))
+# function to generate audio text
+def generate_audio_text(n_figures, n_sliders):
+    tag = ""
+    # loop over figures
+    for ii in range(n_figures):
+        tag = tag + f"""
+            <div id="audioTextFigure{ii+1}OverallId" class="audioTextCl">
+            <!-- audio_text_figure_0{ii+1}_overall -->      
+            </div>
+        """
+        # loop over sliders
+        for jj in range(n_sliders):
+            tag = tag + f"""
+                <div id="audioTextFigure{ii+1}Slider{jj+1}Id" class="audioTextCl">          
+                <!-- audio_text_figure_0{ii+1}_slider_0{jj+1} -->
+                </div>
+            """
+    return tag
+
 # Read the content of the .txt file
 with open('./template-01/input_chunks.txt', 'r') as txt_file:
     txt_content = txt_file.read()
@@ -27,10 +46,6 @@ code_chunks = re.findall(r'<py/(.*?)>', txt_content)
 # Filter out any empty strings
 code_chunks = [chunk.strip() for chunk in code_chunks if chunk.strip()]
 
-# # Print the list of code_chunks
-# print("list all code chunks")
-# print(code_chunks)
-
 # Loop over the code_chunks and perform the operations
 for chunk in code_chunks:
     start_marker = f"<py/{chunk}>"
@@ -42,31 +57,24 @@ for chunk in code_chunks:
     except Exception as e:
         print(f"An error occurred while executing the code: {e}")
 
-print("code chunk element 1")
-print(sliders)
-print(len(sliders))
-print(sliders["value"]["slider1"])
-
+# generate sliders tag
 tag = ""
 for ii in range(len(sliders["slider_value"])):
     if ii == 0:
         tag += "<hr>" + generate_slider(slider_header = sliders["slider_header"][ii], button_header = sliders["button_header"][ii], slider_id = str(ii+1), slider_value_id = str(ii+1), slider_value_max = sliders["slider_value_max"][ii], slider_value = sliders["slider_value"][ii], button_id = str(ii+1))
     else:
         tag += "<br>" + generate_slider(slider_header = sliders["slider_header"][ii], button_header = sliders["button_header"][ii], slider_id = str(ii+1), slider_value_id = str(ii+1), slider_value_max = sliders["slider_value_max"][ii], slider_value = sliders["slider_value"][ii], button_id = str(ii+1))
-
-print("this are the slider tags")
-print(tag)
-
+# add slider tags
 html_content = html_content.replace(f"<!-- slider_inputs -->", tag)
 
+# generate audio tag
+tag = generate_audio_text(n_figures=3, n_sliders=len(sliders["value"]))
+html_content = html_content.replace(f"<!-- audio_text_inputs -->", tag)
 
 # Use regular expressions to find all words between </ and >
 html_chunks = re.findall(r'<html/(.*?)>', txt_content)
 # Filter out any empty strings
 html_chunks = [chunk.strip() for chunk in html_chunks if chunk.strip()]
-
-# # Print the list of text_chunks
-# print(html_chunks)
 
 # Loop over the text_chunks and perform the operations
 for chunk in html_chunks:
@@ -88,11 +96,6 @@ js_block = code_block.strip()
 js_block = js_block.replace('"', "'")
 # Replace the variable assignment to use 'var'
 js_block = js_block.replace("sliders =", "var sliders =")
-
-# print(chunk)
-# print(js_block)
-
-# print(f"// {chunk} //")
 
 html_content = html_content.replace(f"// {chunk} //", js_block)
 
